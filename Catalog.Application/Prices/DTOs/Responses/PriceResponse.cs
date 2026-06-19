19b5f062674fd865365ced123636002ffb0a96a0
Josué <direct.josue@gmail.com>
2026-06-19 16:32:53 -0300

    feat: create price response

---
+++ Catalog.Application/Prices/DTOs/Responses/PriceResponse.cs
@@ -0,0 +1,17 @@
+using Catalog.Domain.Enums;
+
+namespace Catalog.Application.Prices.DTOs.Responses;
+
+public record PriceResponse(
+    string Id, 
+    string Name, 
+    string Currency, 
+    decimal Amount,
+    bool IsActive,
+    string ProductId, 
+    string UserId,
+    PriceFrequency Frequency, 
+    PriceCycle? Cycle,
+    DateTime CreatedAt, 
+    DateTime? UpdatedAt
+);
