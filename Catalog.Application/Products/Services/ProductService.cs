1369694e8866c2d5210acc5bdaf09d1600b85014
Josué <direct.josue@gmail.com>
2026-06-19 16:33:56 -0300

    feat: return price list

--- Catalog.Application/Products/Services/ProductService.cs
+++ Catalog.Application/Products/Services/ProductService.cs
@@ -1,6 +1,8 @@
+using Catalog.Application.Prices.DTOs.Responses;
 using Catalog.Application.Products.DTOs.Requests;
 using Catalog.Application.Products.DTOs.Responses;
 using Catalog.Application.Products.Interfaces;
+using Catalog.Domain.Prices.Entities;
 using Catalog.Domain.Products.Entities;
 using Catalog.Domain.Products.Repositories;
 using Customers.Application.Customers.DTOs.Responses;
@@ -19,13 +21,6 @@ public class ProductService : IProductService
 
     public async Task<Result<ProductResponse>> CreateAsync(string userId, CreateProductRequest request)
     {
-        var productFound = await _productRepository.GetByIdAsync(userId, request.Name);
-
-        if (productFound != null)
-            return Result<ProductResponse>.Failure(
-                "Já existe um produto com o mesmo nome", 
-                ErrorType.Conflict);
-
         var product = new Product(
             request.Name, 
             userId,
@@ -34,8 +29,23 @@ public class ProductService : IProductService
             request.Description, 
             request.Metadata
         );
+
+        if (request.Prices != null && request.Prices.Any())
+        {
+            var prices = request.Prices.Select(item => new Price(
+                item.Name, 
+                item.Amount, 
+                item.Currency, 
+                item.Frequency, 
+                item.Cycle, 
+                product.Id, 
+                userId
+            ));
+            
+            product.SetPrices(prices);
+        }
         
-        var createdProduct = await _productRepository.CreateAsync(product);
+        await _productRepository.CreateAsync(product);
 
         return Result<ProductResponse>.Created(new ProductResponse(
             product.Id, 
@@ -44,6 +54,19 @@ public class ProductService : IProductService
             product.LiveMode, 
             product.IsActive, 
             product.UserId, 
+            product.Prices != null && product.Prices.Any() ? product.Prices.Select(item => new PriceResponse(
+                item.Id, 
+                item.Name, 
+                item.Currency, 
+                item.Amount,
+                item.IsActive,
+                item.ProductId, 
+                item.UserId,
+                item.Frequency, 
+                item.Cycle,
+                item.CreatedAt, 
+                item.UpdatedAt
+            )) : new List<PriceResponse>(),
             product.Metadata, 
             product.CreatedAt, 
             product.UpdatedAt
@@ -64,6 +87,19 @@ public class ProductService : IProductService
             product.LiveMode, 
             product.IsActive, 
             product.UserId, 
+            product.Prices != null && product.Prices.Any() ? product.Prices.Select(item => new PriceResponse(
+                item.Id, 
+                item.Name, 
+                item.Currency, 
+                item.Amount,
+                item.IsActive,
+                item.ProductId, 
+                item.UserId,
+                item.Frequency, 
+                item.Cycle,
+                item.CreatedAt, 
+                item.UpdatedAt
+            )) : new List<PriceResponse>(),
             product.Metadata, 
             product.CreatedAt, 
             product.UpdatedAt
@@ -80,7 +116,20 @@ public class ProductService : IProductService
             item.Description,
             item.LiveMode,
             item.IsActive,
-            item.UserId,
+            item.UserId, 
+            item.Prices != null && item.Prices.Any() ? item.Prices.Select(price => new PriceResponse(
+                price.Id, 
+                price.Name, 
+                price.Currency, 
+                price.Amount,
+                price.IsActive,
+                price.ProductId, 
+                price.UserId,
+                price.Frequency, 
+                price.Cycle,
+                price.CreatedAt, 
+                price.UpdatedAt
+            )) : new List<PriceResponse>(),
             item.Metadata,
             item.CreatedAt,
             item.UpdatedAt
