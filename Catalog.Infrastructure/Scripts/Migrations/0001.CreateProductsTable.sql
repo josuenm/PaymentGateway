3eec861f2ae5d3ffb9c4b5061e18d13b766d402b
Josué <direct.josue@gmail.com>
2026-06-21 02:14:48 -0300

    fix: transform uppercase to pascalcase

--- Catalog.Infrastructure/Scripts/Migrations/0001.CreateProductsTable.sql
+++ Catalog.Infrastructure/Scripts/Migrations/0001.CreateProductsTable.sql
@@ -4,8 +4,8 @@ GO
 CREATE TABLE Products (
     Id VARCHAR(50) NOT NULL, 
     Name VARCHAR(255) NOT NULL, 
-    DESCRIPTION VARCHAR(max) NULL, 
-    METADATA NVARCHAR(max) NULL,
+    Description VARCHAR(max) NULL, 
+    Metadata NVARCHAR(max) NULL,
     UserId VARCHAR(50) NOT NULL, 
     IsActive BIT NOT NULL, 
     LiveMode BIT NOT NULL,
