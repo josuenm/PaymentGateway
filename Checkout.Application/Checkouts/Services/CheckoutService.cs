acfdb36f710d6d46e8938def2449f21691052047
Josué <direct.josue@gmail.com>
2026-06-25 05:01:52 -0300

    feat: call paymentLink and catalog internal api

--- Checkout.Application/Checkouts/Services/CheckoutService.cs
+++ Checkout.Application/Checkouts/Services/CheckoutService.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 using Checkout.Application.Checkouts.DTOs.Responses;
 using Checkout.Application.Checkouts.Interfaces;
 using Shared.Kernel.Results;
@@ -18,25 +20,85 @@ public class CheckoutService : ICheckoutService
     {
         try
         {
-            var client = _httpClientFactory.CreateClient("PaymentLinkClient");
-            var paymentLink = await client
-                .GetFromJsonAsync<PaymentLinkResponse?>("/api/v1/internal/{paymentLinkId}");
+            var jsonOption = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            
+            var paymentLinkClient = _httpClientFactory.CreateClient("PaymentLinkClient");
+            var paymentLinkResponse = await paymentLinkClient.GetAsync($"/api/v1/paymentlink/internal/{paymentLinkId}");
+            var paymentLinkJson = await paymentLinkResponse.Content.ReadAsStringAsync();
+            var paymentLinkResult = JsonSerializer.Deserialize<ResultObject<PaymentLinkResponse>>(
+                paymentLinkJson, 
+                jsonOption
+            );
 
+            if (paymentLinkResult == null)
+                return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter o link de pagamento");
+            
+            if (!paymentLinkResult.Success && paymentLinkResponse.StatusCode == HttpStatusCode.NotFound)
+                return Result<IEnumerable<ItemResponse>>.NotFound("O link de pagamento não existe");
+            
+            if (!paymentLinkResult.Success)
+                return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter o link de pagamento");
+
+            var paymentLink = paymentLinkResult.Data;
+            
             if (paymentLink == null)
-            {
-                return Result<IEnumerable<ItemResponse>>.NotFound("Link de pagamento não encontrado");
-            }
-
+                return Result<IEnumerable<ItemResponse>>.NotFound("O link de pagamento não existe");
+            
             if (!paymentLink.IsActive)
+                return Result<IEnumerable<ItemResponse>>.NotFound("O link de pagamento não existe");
+
+            var paymentLinkItems = paymentLink.Items.ToList();
+            
+            var priceIdList = paymentLinkItems
+                .Select(p => p.PriceId)
+                .ToList();
+            
+            var priceList = string.Join(",", priceIdList);
+            
+            var pricesClient = _httpClientFactory.CreateClient("CatalogClient");
+            var pricesResponse = await pricesClient.GetAsync($"/api/v1/prices/internal?idList={priceList}");
+            var pricesJson = await pricesResponse.Content.ReadAsStringAsync();
+            var pricesResult = JsonSerializer.Deserialize<ResultObject<IEnumerable<PriceResponse>>>(
+                pricesJson, 
+                jsonOption
+            );
+            
+            if (pricesResult == null)
+                return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter itens");
+            
+            if (!pricesResult.Success || pricesResult.Data == null)
+                return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter itens");
+
+            var items = pricesResult.Data.Select(p =>
             {
-                return Result<IEnumerable<ItemResponse>>.NotFound("O link de pagamento não esta mais ativo");
-            }
+                var item = paymentLinkItems.FirstOrDefault(i =>
+                {
+                    Console.WriteLine($"PAYMENT LINK ITEM ID: {i.PriceId} | PRICE ID: {p.Id}");
+                    return i.PriceId == p.Id;
+                });
+
+                if (item == null) return null;
+                
+                return new ItemResponse(
+                    p.ProductId,
+                    p.Id,
+                    p.Name,
+                    item.Quantity,
+                    p.Currency,
+                    p.Amount, 
+                    p.Frequency,
+                    p.Cycle
+                );
+            })
+            .Where(p => p != null)
+            .Select(p => p!)
+            .ToList();
+            
+            return Result<IEnumerable<ItemResponse>>.Ok(items);
         }
         catch (HttpRequestException e)
         {
             return Result<IEnumerable<ItemResponse>>.BadRequest("Erro ao obter o link de pagamento");
         }
-
-        throw new NotImplementedException();
     }
 }
