492d94f3bea9a60ef3f1a6ceb69352da8f0de274
Josué <direct.josue@gmail.com>
2026-06-29 22:57:18 -0300

    feat: create pix payment method

--- src/Services/Checkout/Checkout.Application/Checkouts/Services/CheckoutService.cs
+++ src/Services/Checkout/Checkout.Application/Checkouts/Services/CheckoutService.cs
@@ -1,48 +1,134 @@
-using System.Net;
-using System.Net.Http.Json;
-using System.Text.Json;
+using Checkout.Application.Checkouts.Abstractions;
+using Checkout.Application.Checkouts.DTOs.Requests;
 using Checkout.Application.Checkouts.DTOs.Responses;
 using Checkout.Application.Checkouts.Interfaces;
+using Checkout.Domain.Checkouts.Entities;
+using Checkout.Domain.Checkouts.Repositories;
+using Checkout.Infrastructure.Http.Interfaces;
 using Shared.Kernel.Results;
 
 namespace Checkout.Application.Checkouts.Services;
 
 public class CheckoutService : ICheckoutService
 {
-    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ICheckoutSessionRepository _checkoutSessionRepository;
+    private readonly ICustomerApiClient _customerApiClient;
+    private readonly IPaymentLinkApiClient _paymentLinkApiClient;
+    private readonly IPaymentApiClient _paymentApiClient;
+    private readonly IPriceApiClient _priceApiClient;
 
-    public CheckoutService(IHttpClientFactory httpClientFactory)
+    public CheckoutService(
+        ICheckoutSessionRepository checkoutSessionRepository, 
+        ICustomerApiClient customerApiClient,
+        IPaymentLinkApiClient paymentLinkApiClient, 
+        IPaymentApiClient paymentApiClient,
+        IPriceApiClient priceApiClient
+    )
     {
-        _httpClientFactory = httpClientFactory;
+        _checkoutSessionRepository = checkoutSessionRepository;
+        
+        _customerApiClient = customerApiClient;
+        _paymentLinkApiClient = paymentLinkApiClient;
+        _paymentApiClient = paymentApiClient;
+        _priceApiClient = priceApiClient;
+    }
+
+    public async Task<Result<PixPaymentResponse>> CreatePixPaymentAsync(CreatePaymentRequest paymentRequest)
+    {
+        var paymentLink = await _paymentLinkApiClient.GetAsync(paymentRequest.SourceId);
+        
+        if (paymentLink == null)
+            return Result<PixPaymentResponse>.InternalServerError("Erro ao obter o link de pagamento");
+
+        
+        
+        
+        var customerPayload = new CreateCustomerHttpRequest(
+            paymentRequest.Customer!.Email,
+            paymentRequest.Customer!.Name,
+            paymentRequest.Customer!.TaxId
+        );
+
+        
+        var userId = paymentLink.UserId;
+        var customer = await _customerApiClient.GetOrCreateAsync(userId, customerPayload);
+
+        
+        if (customer == null)
+            return Result<PixPaymentResponse>.InternalServerError("Erro ao obter o cliente");
+        
+        
+        
+        
+        var priceIdList = paymentLink
+            .Items
+            .ToList()
+            .Select(p => p.PriceId)
+            .ToList();
+            
+        var prices = await _priceApiClient.GetManyByIdAsync(priceIdList);
+            
+        if (prices == null)
+            return Result<PixPaymentResponse>.InternalServerError("Erro ao obter itens");
+
+        
+        var total = prices.Sum(p => p.Amount);
+        
+        
+        
+        
+        var paymentPayload = new CreatePixPaymentHttpRequest(
+            new CustomerPixPaymentHttpRequest(
+                customer.Id, 
+                customer.Email, 
+                customer.Name, 
+                customer.TaxId
+            ), 
+            paymentRequest.Method, 
+            total, 
+            "BRL", 
+            userId, 
+            true 
+        );
+        var payment = await _paymentApiClient.CreatePixPaymentAsync(paymentPayload);
+        
+        if (payment == null)
+            return Result<PixPaymentResponse>.InternalServerError("Erro ao gerar cobrança");
+        
+        
+        
+        var paymentId = payment.PaymentId;
+        var qrCodeData = payment.QrCodeData;
+
+        var session = new CheckoutSession(
+            customer.Id, 
+            paymentId, 
+            paymentRequest.SourceId, 
+            total, 
+        "BRL", 
+            userId, 
+            paymentRequest.Method
+        );
+        
+        await _checkoutSessionRepository.CreateAsync(session);
+
+        return Result<PixPaymentResponse>.Created(new PixPaymentResponse(
+            session.Id, 
+            paymentId, 
+            qrCodeData, 
+            13000, 
+            total
+        ));
     }
     
     public async Task<Result<IEnumerable<ItemResponse>>> GetPaymentLinkItemsDetailsAsync(string paymentLinkId)
     {
         try
         {
-            var jsonOption = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-            
-            var paymentLinkClient = _httpClientFactory.CreateClient("PaymentLinkClient");
-            var paymentLinkResponse = await paymentLinkClient.GetAsync($"/api/v1/paymentlink/internal/{paymentLinkId}");
-            var paymentLinkJson = await paymentLinkResponse.Content.ReadAsStringAsync();
-            var paymentLinkResult = JsonSerializer.Deserialize<ResultObject<PaymentLinkResponse>>(
-                paymentLinkJson, 
-                jsonOption
-            );
+            var paymentLink = await _paymentLinkApiClient.GetAsync(paymentLinkId);
 
-            if (paymentLinkResult == null)
-                return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter o link de pagamento");
-            
-            if (!paymentLinkResult.Success && paymentLinkResponse.StatusCode == HttpStatusCode.NotFound)
-                return Result<IEnumerable<ItemResponse>>.NotFound("O link de pagamento não existe");
-            
-            if (!paymentLinkResult.Success)
-                return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter o link de pagamento");
-
-            var paymentLink = paymentLinkResult.Data;
-            
             if (paymentLink == null)
-                return Result<IEnumerable<ItemResponse>>.NotFound("O link de pagamento não existe");
+                return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter o link de pagamento");
             
             if (!paymentLink.IsActive)
                 return Result<IEnumerable<ItemResponse>>.NotFound("O link de pagamento não existe");
@@ -53,23 +139,12 @@ public class CheckoutService : ICheckoutService
                 .Select(p => p.PriceId)
                 .ToList();
             
-            var priceList = string.Join(",", priceIdList);
+            var prices = await _priceApiClient.GetManyByIdAsync(priceIdList);
             
-            var pricesClient = _httpClientFactory.CreateClient("CatalogClient");
-            var pricesResponse = await pricesClient.GetAsync($"/api/v1/prices/internal?idList={priceList}");
-            var pricesJson = await pricesResponse.Content.ReadAsStringAsync();
-            var pricesResult = JsonSerializer.Deserialize<ResultObject<IEnumerable<PriceResponse>>>(
-                pricesJson, 
-                jsonOption
-            );
-            
-            if (pricesResult == null)
+            if (prices == null)
                 return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter itens");
             
-            if (!pricesResult.Success || pricesResult.Data == null)
-                return Result<IEnumerable<ItemResponse>>.InternalServerError("Erro ao obter itens");
-
-            var items = pricesResult.Data.Select(p =>
+            var items = prices.Select(p =>
             {
                 var item = paymentLinkItems.FirstOrDefault(i =>
                 {
