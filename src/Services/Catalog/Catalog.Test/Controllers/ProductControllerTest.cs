17218df6d30675c92a66cb9205c5eb92b901ef89
Josué <direct.josue@gmail.com>
2026-07-01 19:43:15 -0300

    feat: create controller test

--- src/Services/Catalog/Catalog.Test/Controllers/ProductControllerTest.cs
+++ src/Services/Catalog/Catalog.Test/Controllers/ProductControllerTest.cs
@@ -1,5 +1,6 @@
 using Catalog.API.Controllers;
 using Catalog.Application.Prices.DTOs.Requests;
+using Catalog.Application.Prices.DTOs.Responses;
 using Catalog.Application.Products.DTOs.Requests;
 using Catalog.Application.Products.DTOs.Responses;
 using Catalog.Application.Products.Interfaces;
@@ -47,16 +48,32 @@ public class ProductControllerTest
     [Fact]
     public async Task CreateProduct_ValidData_ReturnsCreated()
     {
-        var request = new CreateProductRequest("Produto X", "Exemplo", new List<CreatePriceRequest>());
+        var request = new CreateProductRequest(
+            "Produto X", 
+            "Exemplo", 
+            new List<CreatePriceRequest>()
+        );
 
-        // _productServiceMock
-        //     .Setup(method => method.CreateAsync(
-        //         It.IsAny<string>(), 
-        //         It.IsAny<CreateProductRequest>()
-        //     ))
-        //     .ReturnsAsync();
-        
+        _productServiceMock
+            .Setup(method => method.CreateAsync(
+                It.IsAny<string>(), 
+                It.IsAny<CreateProductRequest>()
+            ))
+            .ReturnsAsync(Result<ProductResponse>.Created(new ProductResponse(
+            "prod_123", 
+            request.Name, 
+            request.Description, 
+            false, 
+            true, 
+            "usr_123", 
+            new List<PriceResponse>(),
+            null, 
+            DateTime.UtcNow, 
+            null
+        )));
+
         var result = await _productsController.CreateAsync("usr_123", request);
+
         var resultObject = Assert.IsType<ObjectResult>(result);
         var resultValue = Assert.IsType<ResultObject<ProductResponse>>(resultObject.Value);
         
@@ -65,5 +82,74 @@ public class ProductControllerTest
         Assert.Null(resultValue.Error);
         Assert.NotNull(resultValue.Data);
         Assert.True(resultValue.Success);
+    }
+    
+    [Fact]
+    public async Task GetAllPaged_ReturnsOk()
+    {
+        var userId = "usr_123";
+        var page = 1;
+        var limit = 10;
+
+        _productServiceMock
+            .Setup(method => method.GetAllPagedAsync(userId, page, limit))
+            .ReturnsAsync(new PagedResponse<ProductResponse>(
+                new List<ProductResponse>(), 
+                0, 
+                page, 
+                1, 
+                limit
+            ));
+        
+        var result = await _productsController.GetAllPagedAsync(userId, page, limit);
+        var resultObject = Assert.IsType<OkObjectResult>(result);
+        var resultValue = Assert.IsType<PagedResponse<ProductResponse>>(resultObject.Value);
+        
+        Assert.Equal(200, resultObject.StatusCode);
+        
+        Assert.Equal(page, resultValue.Page);
+        Assert.Equal(limit, resultValue.Limit);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_WithValidId_ReturnsOk()
+    {
+        var userId = "usr_123";
+        var productId = "prod_123";
+
+        var productResponse = new ProductResponse(
+            productId, 
+            "Produto X", 
+            null, 
+            false, 
+            true, 
+            userId, 
+            new List<PriceResponse>(),
+            null, 
+            DateTime.UtcNow, 
+            null
+        );
+
+        _productServiceMock
+            .Setup(method => method.GetByIdAsync(userId, productId))
+            .ReturnsAsync(Result<ProductResponse>.Ok(productResponse));
+        
+        var result = await _productsController.GetByIdAsync(userId, productId);
+        var resultObject = Assert.IsType<ObjectResult>(result);
+        var resultValue = Assert.IsType<ResultObject<ProductResponse>>(resultObject.Value);
+        
+        Assert.Equal(200, resultObject.StatusCode);
+        
+        Assert.Null(resultValue.Error);
+        Assert.NotNull(resultValue.Data);
+        Assert.True(resultValue.Success);
+        
+        Assert.Equal(productResponse.Id, resultValue.Data.Id);
+        Assert.Equal(productResponse.Name, resultValue.Data.Name);
+        Assert.Equal(productResponse.Description, resultValue.Data.Description);
+        Assert.Equal(productResponse.IsActive, resultValue.Data.IsActive);
+        Assert.Equal(productResponse.UserId, resultValue.Data.UserId);
+        Assert.Equal(productResponse.CreatedAt, resultValue.Data.CreatedAt);
+        Assert.Equal(productResponse.UpdatedAt, resultValue.Data.UpdatedAt);
     }
 }
